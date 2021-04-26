364c03d41405e53d33a3fac9ed0cbbe7e05ba1e5
yunus-atmaca <yunusatmacabm@gmail.com>
2021-04-26 20:42:19 +0300

    add musics and util file

---
+++ app/src/main/java/com/lionbiterclacclac/utils/Constants.java
@@ -0,0 +1,21 @@
+package com.lionbiterclacclac.utils;
+
+import com.lionbiterclacclac.R;
+
+public class Constants {
+    public static final String PREFERENCE_NAME = "LionBitter";
+
+    public static final String KEY_SOUND = "Sound";
+    public static final String KEY_MUSIC = "Music";
+    public static final String KEY_LANGUAGE = "Language";
+
+
+    public static final String LAN_ENG = "eng";
+    public static final String LAN_RU = "ru";
+
+    public static final int BACKGROUND = R.raw.background;
+    public static final int BUTTON = R.raw.button;
+    public static final int BITE_BALL = R.raw.bite_ball;
+    public static final int GAME_OVER = R.raw.game_over;
+    public static final int CLOSE_MOUTH = R.raw.close_mouth;
+}
