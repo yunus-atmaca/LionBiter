2217a9f8f89398cce76bc116f01e1cbe03dfafa4
yunus-atmaca <yunusatmacabm@gmail.com>
2021-04-26 21:03:08 +0300

    add main UI

--- app/src/main/java/com/lionbiterclacclac/MainActivity.java
+++ app/src/main/java/com/lionbiterclacclac/MainActivity.java
@@ -3,12 +3,33 @@ package com.lionbiterclacclac;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+    }
+
+    private void init() {
+
+        findViewById(R.id.play).setOnClickListener(this);
+        findViewById(R.id.settings).setOnClickListener(this);
+        findViewById(R.id.records).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.play){
+
+        }else if(view.getId() == R.id.settings){
+
+        }else if(view.getId() == R.id.records){
+
+        }
     }
 }
